9bb1d44047b3c19c5b041eee2756b4d3b1ac17a7
ChristianTo <christian.to@outlook.com>
2024-05-22 18:11:19 -0500

    open/close

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,32 @@ const initialCards = [
 ]
 
 console.log(initialCards);
+
+
+//selects the edit button in profile
+const profileEditButton = document.querySelector("#profile-edit-button");
+
+//select the modal
+  const profileEditModal = document.querySelector("#profile-edit-modal");
+
+//function to add css to modal
+let openModal = function(){
+  //change the modal css
+  profileEditModal.classList.add("modal_opened");
+}
+
+
+//click edit button to open modal
+profileEditButton.addEventListener("click", openModal);
+
+//select x button
+const modalX = document.querySelector(".modal__close");
+
+//function to remove css to modal
+let closeModal = function(){
+  //change the modal css
+  profileEditModal.classList.remove("modal_opened");
+}
+
+//click x button to close modal
+modalX.addEventListener("click", closeModal);
